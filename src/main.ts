96e2668a1e62006aca71b71e6889da8f8182f203
dawidos13 <d.pniewski@gr8soft.pl>
2022-02-20 16:47:15 +0100

    init

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { appConfig } from './config/app-config';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(appConfig.PORT);
+  console.log('Listening at', appConfig.PORT);
+}
+bootstrap();
