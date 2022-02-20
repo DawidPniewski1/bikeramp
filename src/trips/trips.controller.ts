58317ccedf72cc62cc4631705f0663d1d8e5f73f
dawidos13 <d.pniewski@gr8soft.pl>
2022-02-21 00:31:03 +0100

    add some validation

--- src/trips/trips.controller.ts
+++ src/trips/trips.controller.ts
@@ -1,21 +1,13 @@
+import { TripsDto } from './types/trips-dto';
 import { Trips } from './types/trips.entity';
 import { TripsService } from './trips.service';
-import { Controller, Get } from '@nestjs/common';
-import { getConnection, getRepository } from 'typeorm';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { getRepository } from 'typeorm';
 
 @Controller('trips')
 export class TripsController {
   constructor(private readonly tripsService: TripsService) {}
 
-  @Get('test')
-  async test() {
-    const dbTest = new Trips();
-    dbTest.date = new Date();
-    dbTest.destination_address = 'adr test';
-    dbTest.distance = 10;
-    dbTest.price = 12;
-    dbTest.start_address = 'adr test';
-
-    getRepository(Trips).save(dbTest);
-  }
+  @Post()
+  async addTrip(@Body() add: TripsDto) {}
 }
--- src/trips/types/trips-dto.ts
+++ src/trips/types/trips-dto.ts
@@ -1,6 +1,17 @@
+import { IsDate, IsDateString, IsNotEmpty, IsNumber } from 'class-validator';
+
 export class TripsDto {
+  @IsNotEmpty()
   start_address: string;
+
+  @IsNotEmpty()
   destination_address: string;
+
+  @IsNotEmpty()
+  @IsNumber()
   price: number;
+
+  @IsNotEmpty()
+  @IsDateString()
   date: Date;
 }
